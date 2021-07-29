656f2600dd3bb2b09988812c1a49376b3b27f594
Kholifah Hidayatu Akbar <lekbarun@gmail.com>
2021-07-29 17:03:26 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace SpeechDatasetRecorder
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLoadTranscript = new System.Windows.Forms.Button();
+            this.textBoxTranscriptPath = new System.Windows.Forms.TextBox();
+            this.textBoxSavePath = new System.Windows.Forms.TextBox();
+            this.buttonSavePath = new System.Windows.Forms.Button();
+            this.richTextBoxTranscript = new System.Windows.Forms.RichTextBox();
+            this.buttonRec = new System.Windows.Forms.Button();
+            this.buttonStopRec = new System.Windows.Forms.Button();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.buttonReplay = new System.Windows.Forms.Button();
+            this.dialogFolderSavePath = new System.Windows.Forms.FolderBrowserDialog();
+            this.dialogFileTranscriptPath = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // buttonLoadTranscript
+            // 
+            this.buttonLoadTranscript.Location = new System.Drawing.Point(309, 13);
+            this.buttonLoadTranscript.Name = "buttonLoadTranscript";
+            this.buttonLoadTranscript.Size = new System.Drawing.Size(125, 23);
+            this.buttonLoadTranscript.TabIndex = 0;
+            this.buttonLoadTranscript.Text = "Load Transcript File...";
+            this.buttonLoadTranscript.UseVisualStyleBackColor = true;
+            this.buttonLoadTranscript.Click += new System.EventHandler(this.buttonLoadTranscript_Click);
+            // 
+            // textBoxTranscriptPath
+            // 
+            this.textBoxTranscriptPath.Location = new System.Drawing.Point(13, 13);
+            this.textBoxTranscriptPath.Name = "textBoxTranscriptPath";
+            this.textBoxTranscriptPath.ReadOnly = true;
+            this.textBoxTranscriptPath.Size = new System.Drawing.Size(290, 23);
+            this.textBoxTranscriptPath.TabIndex = 1;
+            // 
+            // textBoxSavePath
+            // 
+            this.textBoxSavePath.Location = new System.Drawing.Point(13, 42);
+            this.textBoxSavePath.Name = "textBoxSavePath";
+            this.textBoxSavePath.ReadOnly = true;
+            this.textBoxSavePath.Size = new System.Drawing.Size(290, 23);
+            this.textBoxSavePath.TabIndex = 3;
+            // 
+            // buttonSavePath
+            // 
+            this.buttonSavePath.Location = new System.Drawing.Point(309, 42);
+            this.buttonSavePath.Name = "buttonSavePath";
+            this.buttonSavePath.Size = new System.Drawing.Size(125, 23);
+            this.buttonSavePath.TabIndex = 2;
+            this.buttonSavePath.Text = "Save Path...";
+            this.buttonSavePath.UseVisualStyleBackColor = true;
+            this.buttonSavePath.Click += new System.EventHandler(this.buttonSavePath_Click);
+            // 
+            // richTextBoxTranscript
+            // 
+            this.richTextBoxTranscript.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.richTextBoxTranscript.Location = new System.Drawing.Point(13, 71);
+            this.richTextBoxTranscript.Name = "richTextBoxTranscript";
+            this.richTextBoxTranscript.ReadOnly = true;
+            this.richTextBoxTranscript.Size = new System.Drawing.Size(421, 138);
+            this.richTextBoxTranscript.TabIndex = 4;
+            this.richTextBoxTranscript.Text = "lorem ipsum";
+            // 
+            // buttonRec
+            // 
+            this.buttonRec.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonRec.ForeColor = System.Drawing.Color.Red;
+            this.buttonRec.Location = new System.Drawing.Point(188, 216);
+            this.buttonRec.Name = "buttonRec";
+            this.buttonRec.Size = new System.Drawing.Size(75, 34);
+            this.buttonRec.TabIndex = 5;
+            this.buttonRec.Text = "REC";
+            this.buttonRec.UseVisualStyleBackColor = true;
+            this.buttonRec.Click += new System.EventHandler(this.buttonRec_Click);
+            // 
+            // buttonStopRec
+            // 
+            this.buttonStopRec.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonStopRec.Location = new System.Drawing.Point(188, 256);
+            this.buttonStopRec.Name = "buttonStopRec";
+            this.buttonStopRec.Size = new System.Drawing.Size(75, 34);
+            this.buttonStopRec.TabIndex = 6;
+            this.buttonStopRec.Text = "STOP";
+            this.buttonStopRec.UseVisualStyleBackColor = true;
+            this.buttonStopRec.Click += new System.EventHandler(this.buttonStopRec_Click);
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonNext.Location = new System.Drawing.Point(269, 215);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(75, 37);
+            this.buttonNext.TabIndex = 7;
+            this.buttonNext.Text = "NEXT";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // buttonReplay
+            // 
+            this.buttonReplay.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonReplay.Location = new System.Drawing.Point(107, 215);
+            this.buttonReplay.Name = "buttonReplay";
+            this.buttonReplay.Size = new System.Drawing.Size(75, 37);
+            this.buttonReplay.TabIndex = 9;
+            this.buttonReplay.Text = "REPLAY";
+            this.buttonReplay.UseVisualStyleBackColor = true;
+            this.buttonReplay.Click += new System.EventHandler(this.buttonReplay_Click);
+            // 
+            // dialogFolderSavePath
+            // 
+            this.dialogFolderSavePath.RootFolder = System.Environment.SpecialFolder.CommonDocuments;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 294);
+            this.Controls.Add(this.buttonReplay);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.buttonStopRec);
+            this.Controls.Add(this.buttonRec);
+            this.Controls.Add(this.richTextBoxTranscript);
+            this.Controls.Add(this.textBoxSavePath);
+            this.Controls.Add(this.buttonSavePath);
+            this.Controls.Add(this.textBoxTranscriptPath);
+            this.Controls.Add(this.buttonLoadTranscript);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLoadTranscript;
+        private System.Windows.Forms.TextBox textBoxTranscriptPath;
+        private System.Windows.Forms.TextBox textBoxSavePath;
+        private System.Windows.Forms.Button buttonSavePath;
+        private System.Windows.Forms.RichTextBox richTextBoxTranscript;
+        private System.Windows.Forms.Button buttonRec;
+        private System.Windows.Forms.Button buttonStopRec;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Button buttonReplay;
+        private System.Windows.Forms.FolderBrowserDialog dialogFolderSavePath;
+        private System.Windows.Forms.OpenFileDialog dialogFileTranscriptPath;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,194 @@
+﻿using NAudio.Wave;
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SpeechDatasetRecorder
+{
+    public partial class Form1 : Form
+    {
+        private int lineNum;
+        private WaveIn waveSource = null;
+        private WaveFileWriter waveFile = null;
+        private WaveOutEvent playRecord;
+        private AudioFileReader audioFile;
+
+        public Form1()
+        {
+            InitializeComponent();
+            buttonReplay.Enabled = false;
+            buttonStopRec.Enabled = false;
+            buttonNext.Enabled = false;
+        }
+
+        private void buttonLoadTranscript_Click(object sender, EventArgs e)
+        {
+            dialogFileTranscriptPath.Filter = "Text File|*.txt";
+            if(dialogFileTranscriptPath.ShowDialog() == DialogResult.OK)
+            {
+                textBoxTranscriptPath.Text = dialogFileTranscriptPath.FileName;
+                using (StreamReader file = new StreamReader(textBoxTranscriptPath.Text))
+                {
+                    for (int i = 0; i < 1; i++)
+                    {
+                        richTextBoxTranscript.Text = file.ReadLine();
+                    }
+                }
+            }
+        }
+
+        public void buttonSavePath_Click(object sender, EventArgs e)
+        {
+            if(dialogFolderSavePath.ShowDialog() == DialogResult.OK)
+            {
+                textBoxSavePath.Text = dialogFolderSavePath.SelectedPath;
+                if (!File.Exists(@textBoxSavePath.Text + @"\checkpoint.txt"))
+                {
+                    using (TextWriter newCheck = new StreamWriter(@textBoxSavePath.Text + @"\checkpoint.txt"))
+                    {
+                        newCheck.WriteLine("0");
+                        newCheck.Close();
+                    }
+                }
+                using (StreamReader checkpoint = new StreamReader(@textBoxSavePath.Text + @"\checkpoint.txt"))
+                {
+                    int.TryParse(checkpoint.ReadLine(), out lineNum);
+                }
+                using (StreamReader transcript = new StreamReader(@textBoxTranscriptPath.Text))
+                {
+                    for (int i = 0; i <= lineNum; i++)
+                    {
+                        richTextBoxTranscript.Text = transcript.ReadLine();
+                    }
+                }
+                Directory.CreateDirectory(@textBoxSavePath.Text + @"\clips");
+            }
+        }
+
+        private void buttonRec_Click(object sender, EventArgs e)
+        {
+            buttonRec.Enabled = false;
+            buttonReplay.Enabled = false;
+            buttonNext.Enabled = false;
+            buttonStopRec.Enabled = true;
+
+            //Arigathanks gozaimuch Corey (https://stackoverflow.com/a/17983876/15597379)
+            waveSource = new WaveIn();
+            waveSource.WaveFormat = new WaveFormat(16000, 1);
+
+            waveSource.DataAvailable += new EventHandler<WaveInEventArgs>(waveSource_DataAvailable);
+            waveSource.RecordingStopped += new EventHandler<StoppedEventArgs>(waveSource_RecordingStopped);
+
+            waveFile = new WaveFileWriter(@textBoxSavePath.Text + @"\clips\Test.wav", waveSource.WaveFormat);
+
+            waveSource.StartRecording();
+        }
+
+        private void buttonStopRec_Click(object sender, EventArgs e)
+        {
+            buttonRec.Enabled = true;
+            buttonReplay.Enabled = true;
+            buttonNext.Enabled = true;
+            buttonStopRec.Enabled = false;
+            buttonRec.Text = "Retry";
+            waveSource.StopRecording();
+            //stop
+        }
+
+        void waveSource_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            if (waveFile != null)
+            {
+                waveFile.Write(e.Buffer, 0, e.BytesRecorded);
+                waveFile.Flush();
+            }
+        }
+
+        void waveSource_RecordingStopped(object sender, StoppedEventArgs e)
+        {
+            if (waveSource != null)
+            {
+                waveSource.Dispose();
+                waveSource = null;
+            }
+
+            if (waveFile != null)
+            {
+                waveFile.Dispose();
+                waveFile = null;
+            }
+
+            buttonRec.Enabled = true;
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            buttonRec.Enabled = true;
+            buttonReplay.Enabled = false;
+            buttonNext.Enabled = false;
+            buttonStopRec.Enabled = false;
+            buttonRec.Text = "REC";
+            if(!File.Exists(@textBoxSavePath.Text + @"\main.csv"))
+            {
+                using (TextWriter CSV = new StreamWriter(@textBoxSavePath.Text + @"\main.csv"))
+                {
+                    CSV.WriteLine("wav_filename,wav_size,transcript");
+                    CSV.Close();
+                }
+            }
+            File.Move(@textBoxSavePath.Text + @"\clips\Test.wav", String.Format(@"{0}\clips\{1}.wav", @textBoxSavePath.Text, lineNum));
+            using (TextWriter CSV = new StreamWriter(@textBoxSavePath.Text + @"\main.csv", true))
+            {
+                long fileSize = new System.IO.FileInfo(String.Format(@"{0}\clips\{1}.wav", @textBoxSavePath.Text, lineNum)).Length;
+                CSV.WriteLine(String.Format("{0}.wav,{1},{2}", lineNum, fileSize, richTextBoxTranscript.Text));
+                CSV.Close();
+            }
+            lineNum++;
+            using (TextWriter checkpoint = new StreamWriter(@textBoxSavePath.Text + @"\checkpoint.txt"))
+            {
+                checkpoint.WriteLine(lineNum);
+                checkpoint.Close();
+            }
+            using (StreamReader transcript = new StreamReader(@textBoxTranscriptPath.Text))
+            {
+                for (int i = 0; i <= lineNum; i++)
+                {
+                    richTextBoxTranscript.Text = transcript.ReadLine();
+                }
+            }
+        }
+
+        private void buttonReplay_Click(object sender, EventArgs e)
+        {
+            buttonRec.Enabled = false;
+            buttonReplay.Enabled = false;
+            buttonNext.Enabled = false;
+            buttonStopRec.Enabled = false;
+            buttonRec.Text = "Retry";
+            
+            if (playRecord == null)
+            {
+                playRecord = new WaveOutEvent();
+                playRecord.PlaybackStopped += OnPlaybackStopped;
+            }
+            if (audioFile == null)
+            {
+                audioFile = new AudioFileReader(@textBoxSavePath.Text + @"\clips\Test.wav");
+                playRecord.Init(audioFile);
+            }
+            playRecord.Play();
+        }
+
+        private void OnPlaybackStopped(object sender, StoppedEventArgs args)
+        {
+            playRecord.Dispose();
+            playRecord = null;
+            audioFile.Dispose();
+            audioFile = null;
+            buttonRec.Enabled = true;
+            buttonReplay.Enabled = true;
+            buttonNext.Enabled = true;
+            buttonStopRec.Enabled = true;
+        }
+    }
+}
